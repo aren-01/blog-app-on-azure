a8dc4feb8c76bd261ea45973eafc3c3aa02d5218
Aren Aksoy <arenaksy@gmail.com>
2026-05-05 15:50:00 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ provider "azurerm" {
 variable "location" {
   description = "Azure region for all resources."
   type        = string
-  default     = "eastus"
+  default     = "centralus"
 }
 
 variable "client_ip_address" {
